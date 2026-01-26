842fbc082f05d1fe968a2b29e5fe75e12772863b
Sword <330132662@qq.com>
2026-01-26 15:11:21 +0800

    转Kotlin

--- app/src/main/java/com/hjq/demo/http/WebSocketManager.kt
+++ app/src/main/java/com/hjq/demo/http/WebSocketManager.kt
@@ -1,14 +1,10 @@
 package com.hjq.demo.http
 
-import android.os.CountDownTimer
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.text.TextUtils
-import android.util.ArrayMap
-import androidx.annotation.NonNull
 import com.hjq.demo.manager.MmkvUtil
-import com.hjq.demo.other.AppConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
@@ -17,13 +13,14 @@ import okhttp3.WebSocketListener
 import okio.ByteString
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
+import kotlin.coroutines.CoroutineContext
 
 /**
  * 本工程接口文档  https://doc.weixin.qq.com/doc/w3_AD4A9AYCAIA7ZlQL0Y1RKKc5qXpbg?scode=AH4AwAcNAAg4xVDNbFAEQA9AYCAIA
  *
  * https://blog.csdn.net/fengyeNom1/article/details/115183848
  */
-class WebSocketManager private constructor() : WebSocketListener() {
+class WebSocketManager private constructor() : WebSocketListener(), CoroutineScope {
 
     private val NORMAL_CLOSURE_STATUS = 1000
 
@@ -36,45 +33,15 @@ class WebSocketManager private constructor() : WebSocketListener() {
     private var _isConnected = false
     private var sClient: OkHttpClient? = null
     private var sWebSocket: WebSocket? = null
-    private val ACTION_EMPTYMSG = 1400
-    private val ACTION_FAILURE = 1401
 
-    private val handler = object : Handler(Looper.getMainLooper()) {
-        override fun handleMessage(@NonNull msg: Message) {
-            super.handleMessage(msg)
-            when (msg.what) {
-                /*ACTION_EMPTYMSG -> {
-                    val params = ArrayMap<String, String>()
-                    params["type"] = "3"
-                    handler.sendEmptyMessageDelayed(ACTION_EMPTYMSG, 5000)
-                }
-                ACTION_FAILURE -> {
-                    handler.sendEmptyMessageDelayed(ACTION_FAILURE, 10 * 1000)
-                    if (canRetry) {
-                        countDownTimer?.start()
-                        connectWebSocket()
-                    }
-                }
-                else -> {}*/
-            }
-        }
-    }
+    // 协程作用域
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = kotlinx.coroutines.Dispatchers.Main + job
 
-    private var countDownTimer: CountDownTimer? = null
+    private var heartbeatJob: Job? = null
+    private var retryJob: Job? = null
     private var canRetry = true
-
-    init {
-        val timeLong = 3 * 1000
-        countDownTimer = object : CountDownTimer(timeLong.toLong(), 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                canRetry = false
-            }
-
-            override fun onFinish() {
-                canRetry = true
-            }
-        }
-    }
 
     fun connectWebSocket() {
         if (!_isConnected) {
@@ -87,7 +54,7 @@ class WebSocketManager private constructor() : WebSocketListener() {
                     .build()
             }
             if (sWebSocket == null) {
-                val url = socketURL  
+                val url = socketURL ?: return
                 val request = Request.Builder().url(url).build()
                 sWebSocket = sClient!!.newWebSocket(request, this)
             }
@@ -98,20 +65,20 @@ class WebSocketManager private constructor() : WebSocketListener() {
     }
 
     fun sendData(info: String?): Boolean {
-        if (sWebSocket != null && !TextUtils.isEmpty(info)) {
+        if (sWebSocket != null && !info.isNullOrEmpty()) {
             return sWebSocket!!.send(info)
         }
         return false
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
-        if (!TextUtils.isEmpty(text)) {
+        if (text.isNotEmpty()) {
             onGetMsg(text)
         }
     }
 
     override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
-        if (!TextUtils.isEmpty(bytes.hex())) {
+        if (bytes.hex().isNotEmpty()) {
             onGetMsg(bytes.hex())
         }
     }
@@ -130,22 +97,64 @@ class WebSocketManager private constructor() : WebSocketListener() {
         sWebSocket?.close(NORMAL_CLOSURE_STATUS, "Goodbye!")
         sWebSocket = null
         sClient = null
-        handler.removeMessages(ACTION_EMPTYMSG)
-        handler.sendEmptyMessage(ACTION_FAILURE)
+
+        // 取消心跳
+        heartbeatJob?.cancel()
+        heartbeatJob = null
+
+        // 启动重试逻辑
+        scheduleRetry()
     }
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
         Timber.d("onOpen ")
         _isConnected = true
-        handler.removeMessages(ACTION_FAILURE)
-        handler.sendEmptyMessage(ACTION_EMPTYMSG)
+
+        // 取消重试
+        retryJob?.cancel()
+        retryJob = null
+
+        // 启动心跳
+        startHeartbeat()
+    }
+
+    private fun startHeartbeat() {
+        heartbeatJob = launch {
+            while (_isConnected) {
+                delay(5000)
+                if (_isConnected) {
+                    // 心跳包 - 可根据需要发送数据
+                    // val params = mapOf("type" to "3")
+                    // sendData(Gson().toJson(params))
+                }
+            }
+        }
+    }
+
+    private fun scheduleRetry() {
+        if (retryJob?.isActive == true) return
+
+        // 先禁用重试3秒
+        canRetry = false
+        launch {
+            delay(3000)
+            canRetry = true
+        }
+
+        retryJob = launch {
+            delay(10 * 1000)
+            if (canRetry) {
+                connectWebSocket()
+            }
+        }
     }
 
     fun closeWebSocket() {
+        heartbeatJob?.cancel()
+        retryJob?.cancel()
         sWebSocket?.close(NORMAL_CLOSURE_STATUS, "Goodbye!")
         sWebSocket = null
         sClient = null
-        handler.removeCallbacksAndMessages(null)
     }
 
     fun onGetMsg(json: String) {
@@ -153,7 +162,7 @@ class WebSocketManager private constructor() : WebSocketListener() {
     }
 
     fun reqLogin() {
-        val params = ArrayMap<String, String>()
+        val params = mutableMapOf<String, String>()
         params["type"] = "1"
         val id = MmkvUtil.getString(MmkvUtil.MN, "")
         params["sn"] = id ?: ""
